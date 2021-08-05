cfc11efb8ed7ee5a5785e33ceac6e1ba2a042cd1
funeconomist <gary.vartanian@gmail.com>
2021-08-04 20:54:19 -0700

    Update GDP_Data_Viz.R
    got it working only to lose it again.
    Suspect that the dpylr filter has something to do with it. Try shutting off all packages and redoing. Got it to work 4 times before it shut down.
    ALWAYS ALWAYS ALWAYS COMMIT after every success.

--- EDA Data/GDP_Data_Viz.R
+++ EDA Data/GDP_Data_Viz.R
@@ -272,11 +272,110 @@ View(GDP_growth_5_year_avg %>%
 #### Inflation 
 # FP.CPI.TOTL.ZG
 
+#View(WDI_GDP_per_capita)
+
+library(tidysynth)
+library(tidyverse)
+
+check_list <- WDI_GDP_per_capita %>% filter(year > 2000) %>% drop_na() %>% group_by(country) %>% count() 
+check_list2 <- check_list %>% filter(n == 20) %>% select(country)
+top50 <- c("Burundi", "China", "Myanmar", "Ethiopia", "Congo, Dem. Rep.", 
+           "Sierra Leone", "Mozambique", "Malawi", "Rwanda", "Macao SAR, China", 
+           "Niger", "Nepal", "Central African Republic", "Cambodia", "Madagascar", 
+           "Tajikistan", "Liberia", "Bangladesh", "Burkina Faso", 
+           "Togo", "Guinea-Bissau", "Lao PDR", "Tanzania", 
+            "Uganda", "Mongolia", 
+           "Chad", "Timor-Leste", 
+           "Kyrgyz Republic", "Guinea", "Mali", "Uzbekistan", "Vietnam", 
+           "India", "Sudan", 
+           "Ghana",  "Kenya", "Zambia", "Lesotho", 
+           "Bhutan", "Pakistan", "Haiti", "Sri Lanka")
 
 
+inputted_data <-  WDI_GDP_per_capita %>% filter(year >= 2000) %>% drop_na() %>% filter(country %in% unlist(check_list2$country)) %>%
+  #filter(country %in% top50) %>% 
+  arrange(country, year) 
+
+smoking_out <-
+  inputted_data %>% 
+  # initial the synthetic control object
+  synthetic_control(outcome = GDP_per_capita_2010_USD, # outcome
+                    unit = country, # unit index in the panel data
+                    time = year, # time index in the panel data
+                    i_unit = "Armenia", # unit where the intervention occurred
+                    i_time = 2016, # time period when the intervention occurred
+                    generate_placebos=T # generate placebo synthetic controls (for inference)
+  ) %>%
+  
+  generate_predictor(time_window = 2000:2016, 
+  mean_GDP_per_capita_2010_USD = mean(GDP_per_capita_2010_USD, na.rm = TRUE)) %>% 
+  
+    generate_predictor(time_window = 2009,
+                       cigsale_1980 = GDP_per_capita_2010_USD) %>%
+    generate_predictor(time_window = 2014,
+                       cigsale_1988 = GDP_per_capita_2010_USD) %>% 
+
+  # Generate the fitted weights for the synthetic control
+  generate_weights(optimization_window = 2001:2016, margin_ipop = .02,sigf_ipop = 7,bound_ipop = 6 ) %>%
+  
+  # Generate the synthetic control
+  generate_control()
 
 
+smoking_out %>% plot_trends()
+smoking_out %>% plot_differences()
+smoking_out %>% plot_weights()
 
+# It's actually doing fine, but for whatever reason, its just reading the observed in backwards.
+
+
+library(tidyverse)
+library(tidysynth)
+
+data("smoking")
+smoking_out2 <-
+  
+  smoking %>%
+  
+  # initial the synthetic control object
+  synthetic_control(outcome = cigsale, # outcome
+                    unit = state, # unit index in the panel data
+                    time = year, # time index in the panel data
+                    i_unit = "California", # unit where the intervention occurred
+                    i_time = 1988, # time period when the intervention occurred
+                    generate_placebos=T # generate placebo synthetic controls (for inference)
+  ) %>%
+  
+  # Generate the aggregate predictors used to fit the weights
+  
+  # average log income, retail price of cigarettes, and proportion of the
+  # population between 15 and 24 years of age from 1980 - 1988
+  generate_predictor(time_window = 1980:1988,
+                     ln_income = mean(lnincome, na.rm = T),
+                     ret_price = mean(retprice, na.rm = T),
+                     youth = mean(age15to24, na.rm = T)) %>%
+  
+  # average beer consumption in the donor pool from 1984 - 1988
+  generate_predictor(time_window = 1984:1988,
+                     beer_sales = mean(beer, na.rm = T)) %>%
+  
+  # Lagged cigarette sales 
+  generate_predictor(time_window = 1975,
+                     cigsale_1975 = cigsale) %>%
+  generate_predictor(time_window = 1980,
+                     cigsale_1980 = cigsale) %>%
+  generate_predictor(time_window = 1988,
+                     cigsale_1988 = cigsale) %>%
+  
+  
+  # Generate the fitted weights for the synthetic control
+  generate_weights(optimization_window = 1970:1988, # time to use in the optimization task
+                   margin_ipop = .02,sigf_ipop = 7,bound_ipop = 6 # optimizer options
+  ) %>%
+  
+  # Generate the synthetic control
+  generate_control()
+smoking_out2 %>% plot_trends()
 
 
 
